619de6be8877c2b4bf8ef56faaa40c9c9983fdc2
Yves Sagnieres <sagnieres.yves@gmail.com>
2020-05-09 12:53:34 -0400

    Added wind and humidity data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,12 +7,12 @@ const forecast = (long, lat, callback) => {
   request({ url, json: true }, (error, { body }) => {
 
     if (error) {
+      //first is error, second is body
       callback('Unable to connect to API', undefined)
     } else if (body.error) {
       callback('Unable to find location', undefined)
     } else {
-      callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees and it feels like ' + body.current.feelslike + ' degrees.'
-      )
+      callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees and it feels like ' + body.current.feelslike + ' degrees. The humidity is ' + body.current.humidity + '%, and the wind speed is ' + body.current.wind_speed + ' coming from ' + body.current.wind_degree + body.current.wind_dir + '.')
   }
   })
 }
